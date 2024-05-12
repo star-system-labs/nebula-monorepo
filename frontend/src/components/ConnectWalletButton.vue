eacc748a7c68b6ea99c89ec35e5bb55d06cadab3
0xcircuitbreaker <44181392+0xcircuitbreaker@users.noreply.github.com>
2024-05-12 16:22:46 -0400

    Frontend: fix connect wallet text

--- frontend/src/components/ConnectWalletButton.vue
+++ frontend/src/components/ConnectWalletButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button @click="connect" class="bg-gradient-to-r from-indigo-800 via-indigo-500 to-purple-900 text-yellow-300 px-8 py-3 rounded-xl border-none font-origin font-semibold cursor-pointer transition-opacity hover:opacity-80">
-    {{ $t('message.connectwallet') }}
+    {{ $t('message.connect') }}
   </button>
 </template>
 
