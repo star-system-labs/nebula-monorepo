0b8448bb1e1aea6f58d862ecf010a8e8585d5d1f
0xcircuitbreaker <44181392+0xcircuitbreaker@users.noreply.github.com>
2024-12-16 14:07:40 -0500

    [dev] bump logout time from 5 mins to 1 hour

--- frontend/nebula-pitchdeck/src/App.vue
+++ frontend/nebula-pitchdeck/src/App.vue
@@ -151,7 +151,7 @@ const startLogoutTimer = () => {
   clearLogoutTimer();
   logoutTimer = setTimeout(() => {
     logout();
-  }, 5 * 60 * 1000);
+  }, 60 * 60 * 1000);
 };
 
 const clearLogoutTimer = () => {
